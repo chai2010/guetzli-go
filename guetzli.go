07533be1e4d726ad4b024153829f312d76bc95c1
chai2010 <chaishushan@gmail.com>
2017-03-17 23:34:19 +0800

    zz

---
+++ guetzli.go
@@ -0,0 +1,6 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Guetzli perceptual JPEG encoder for Go.
+package guetzli // import "github.com/chai2010/guetzli-go"
---
+++ hello.go
@@ -0,0 +1,15 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	_ "github.com/chai2010/guetzli-go"
+)
+
+func main() {
+	println("TODO")
+}
