aeb8fe08f1de6d01ae33a54271403ae7417d8a21
chai2010 <chaishushan@gmail.com>
2017-03-17 23:16:37 +0800

    c++ remove test code

--- internal/guetzli/hello.cc
+++ internal/guetzli/hello.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include "./png/lodepng.h"
+#include "./lodepng.h"
 
 #include <stdint.h>
 #include <stdio.h>
